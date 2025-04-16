9cd09845a0faf9d9aa1195df51c3f82208b01f98
chandan.thakur1 <chandan.thakur1@go-yubi.com>
2025-04-16 23:34:57 +0530

    first commit

---
+++ src/test/java/com/yubi/coverage/CoverageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubi.coverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
